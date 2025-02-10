e38fcff00fe9c52a6ca5e1874e5defdc462fbbef
Alonso <abrahamreynoso64@gmail.com>
2025-02-09 23:17:18 -0600

    Refactorizando código para quitar animación innecesaria

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,23 +1,14 @@
 AOS.init();
 
-let timeTimeOut = 3500;
-let time = 300;
-
-const animation_1 = () => {
-   document.querySelector("#intro_logo").classList.add("d-none");
-   // document.querySelector("#hero").classList.remove("d-none");
-}
-const animation_2 = () => {
+const rollingImages = () => {
    document.querySelector("#hero").classList.add("fade_in");
    document.querySelector("#img_hero_1").classList.add("img_left_right");
    document.querySelector("#img_hero_2").classList.add("img_right_left");
 }
-const animation_3 = () => {
+const floatingLogo = () => {
    document.querySelector("#span_icon").classList.remove("d-none");
    document.querySelector("body").classList.remove("scroll-y-none");
 }
-
-
 
 
 //#region EFECTO PARALLAX
@@ -26,8 +17,6 @@ window.onscroll = function () {
    let position = window.pageYOffset || document.documentElement.scrollTop;
    let img_hamburger_1 = document.getElementById("img_hamburger_1");
    let section_hamburgers = document.getElementById("section_hamburgers");
-   // console.log(section_hamburgers.style);
-   // img_hamburger_1.style.bottom = `${position * 0.01}px` ;
 }
 //#endregion EFECTO PARALLAX
 
@@ -40,19 +29,18 @@ favicon.href = logo;
 //#endregion DETETCTAR DARK-MODE EN EL NAVEGADOR
 
 
-
-
 document.addEventListener("DOMContentLoaded", function() {
    document.querySelector("body").classList.add("scroll-y-none");
    document.querySelector("#img_hero_1").classList.remove("img_left_right");
    document.querySelector("#img_hero_2").classList.remove("img_right_left");
 
-   animation_3();
-
-   const animations = () => {
-      setTimeout(() => animation_1(), timeTimeOut);
-      setTimeout(() => animation_2(), timeTimeOut += time);
-   }
-   animations();
-
+   floatingLogo();
+   rollingImages();
 })
+
+
+// Inserta de manera dinamica el link con stylesheet una vez se haya pre-cargado
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://site-assets.fontawesome.com/releases/v6.1.0/css/all.css';
+document.head.appendChild(link);
